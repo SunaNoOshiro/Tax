df2d9eb087982e0d704da00ed3558f06b35419a9
Suna_no_Oshiro <program.master.pero@gmail.com>
2014-10-20 00:07:55 +0300

    delete old packages

--- GreatTask1/src/com/epam/ryndych/task1/menu/command/Command.java
+++
@@ -1,8 +0,0 @@
-package com.epam.ryndych.task1.menu.command;
-
-public interface Command {
-
-	public void execute();
-
-	public String name();
-}
--- GreatTask1/src/com/epam/ryndych/task1/tax/IncomeFromMainJob.java
+++
@@ -1,5 +0,0 @@
-package com.epam.ryndych.task1.tax;
-
-public class IncomeFromMainJob {
-
-}
